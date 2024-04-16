23ed2b2ba0b156156a5427c30640aa2bbd0bdc12
saurabh gupta <sg072106@gmail.com>
2024-04-16 13:59:12 +0530

    change 1.1

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = () => {
   return (
     <>
         <div className='navbar flex justify-between bg-red-200'>
-            <img className='logo h-8' src='../images/logo.png'></img>
+            <img className='logo h-8' src='../img/logo.png'></img>
 
             <ul className='navtool w-40 flex justify-around items-center'>
             <Link to="/">HOME</Link>
@@ -12,7 +12,7 @@ const Navigation = () => {
             <Link to="/cart">
                 <div className='cartbg bg-orange-500 px-2 py-1 rounded-lg'>
                 <span>10</span>
-                <img className='cart h-4 inline-block' src='../images/cart.png'></img>
+                <img className='cart h-4 inline-block' src='../img/cart.png'></img>
                 </div>
             </Link>
             </ul>
